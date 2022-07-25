5f3c97b50af57d89ceaa415c7462d3699f2cdac7
VishalChhadekar <104685183+VishalChhadekar@users.noreply.github.com>
2022-07-25 17:55:49 +0530

    added

--- src/main/java/com/spring/ORM/SpringFrameworkORM/App.java
+++ src/main/java/com/spring/ORM/SpringFrameworkORM/App.java
@@ -1,5 +1,7 @@
 package com.spring.ORM.SpringFrameworkORM;
 
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.spring.ORM.SpringFrameworkORM.Dao.EmployeeDao;
@@ -9,18 +11,52 @@ import com.spring.ORM.SpringFrameworkORM.entities.Employee;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Program Initiated!" );
-        ApplicationContext con = new  ClassPathXmlApplicationContext("Config.xml");
-        EmployeeDao employeeDao = con.getBean("employeeDao", EmployeeDao.class);
-        Employee emp = new Employee();
-        emp.setEmpName("Lucifer");
-        emp.setEmpCity("Lux, L. A");
-        int save = employeeDao.save(emp);
-        System.out.println("Record inserted: "+ save);
-                
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Program Initiated!");
+		ApplicationContext con = new ClassPathXmlApplicationContext("Config.xml");
+		EmployeeDao employeeDao = con.getBean("employeeDao", EmployeeDao.class);
+		// SAVE
+//		Employee emp = new Employee();
+//        emp.setEmpName("SandMan");
+//        emp.setEmpCity("Hevan");
+//        int save = employeeDao.save(emp);
+//        System.out.println("Record inserted: "+ save);
+
+		// READ
+//		Employee emp = employeeDao.getEmployee(1);
+//		System.out.println(emp.getEmpId());
+//		System.out.println(emp.getEmpName());
+//		System.out.println(emp.getEmpCity());
+
+		// GET ALL EMPLOYEES
+//		List<Employee> empList = employeeDao.getAllEmployees();
+//		for (Employee emp : empList) {
+//			System.out.println("Emp ID: "+ emp.getEmpId());
+//			System.out.println("Emp Name: "+ emp.getEmpName());
+//			System.out.println("Emp City: "+ emp.getEmpCity());
+//			System.out.println();
+//		}
+		//DELETE
+//		employeeDao.deleteEmployee(4);
+//		System.out.println("Employee deleted.");
+		
+		//UPDATE	
+		Employee emp =employeeDao.getEmployee(2);
+		emp.setEmpName("Morphious");
+		employeeDao.updateEmployee(emp);
+		System.out.println("Employee updated");
+
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/spring/ORM/SpringFrameworkORM/Dao/EmployeeDao.java
+++ src/main/java/com/spring/ORM/SpringFrameworkORM/Dao/EmployeeDao.java
@@ -1,5 +1,7 @@
 package com.spring.ORM.SpringFrameworkORM.Dao;
 
+import java.util.List;
+
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,6 +14,25 @@ public class EmployeeDao {
 	@Transactional
 	public int save(Employee emp) {
 		return (Integer) this.hibernateTemplate.save(emp);
+	}
+
+	public Employee getEmployee(int empId) {
+		return hibernateTemplate.get(Employee.class, empId);
+	}
+
+	public List<Employee> getAllEmployees() {
+		return hibernateTemplate.loadAll(Employee.class);
+	}
+
+	@Transactional
+	public void deleteEmployee(int empId) {
+		Employee emp = hibernateTemplate.get(Employee.class, empId);
+		hibernateTemplate.delete(emp);
+	}
+
+	@Transactional
+	public void updateEmployee(Employee emp) {
+		hibernateTemplate.update(emp);
 	}
 
 	public HibernateTemplate getHibernateTemplate() {
